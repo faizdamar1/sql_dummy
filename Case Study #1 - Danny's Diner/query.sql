33827f31a4089e105e706bf55dd943267706bda1
faiz damar <faizdamar123@gmail.com>
2024-08-12 12:33:25 +0700

    dannys dinner no 3

--- Case Study #1 - Danny's Diner/query.sql
+++ Case Study #1 - Danny's Diner/query.sql
@@ -9,3 +9,15 @@ GROUP BY s.customer_id
 SELECT s.customer_id, COUNT(DISTINCT s.order_date) days
 FROM 1_sales s
 GROUP BY s.customer_id
+
+-- 3. What was the first item from the menu purchased by each customer?
+WITH CTE as (
+	SELECT s.customer_id, s.order_date, m.product_name,
+	RANK() OVER(PARTITION BY customer_id ORDER BY order_date ASC) rnk,
+	ROW_NUMBER() OVER(PARTITION BY customer_id ORDER BY order_date ASC) rn
+	FROM 1_sales s
+	INNER JOIN 1_menu as m ON s.product_id = m.product_id
+)
+SELECT customer_id, product_name, order_date
+FROM CTE
+WHERE rn = 1 
