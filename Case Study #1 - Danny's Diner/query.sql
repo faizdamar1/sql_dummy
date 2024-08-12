008684008a452e1a1dc8d66e431f476b5c7ef313
faiz damar <faizdamar123@gmail.com>
2024-08-12 11:58:57 +0700

    dannys dinner no 2

--- Case Study #1 - Danny's Diner/query.sql
+++ Case Study #1 - Danny's Diner/query.sql
@@ -5,3 +5,7 @@ FROM 1_sales s
 INNER JOIN 1_menu as m ON s.product_id = m.product_id
 GROUP BY s.customer_id
 
+-- 2. How many days has each customer visited the restaurant?
+SELECT s.customer_id, COUNT(DISTINCT s.order_date) days
+FROM 1_sales s
+GROUP BY s.customer_id
