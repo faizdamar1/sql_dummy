5688e5f539a083be829da6e32d843b9bb83459dc
faizdamar1 <faizdamar123@gmail.com>
2024-08-19 13:56:57 +0700

    dannys dunner no 19

--- Case Study #1 - Danny's Diner/query.sql
+++ Case Study #1 - Danny's Diner/query.sql
@@ -110,5 +110,52 @@
 	WHERE order_date >= join_date
 	GROUP BY s.customer_id
 
--- 9. If each $1 spent equates to 10 points and sushi has a 2x points multiplier 1. how many points would each customer have?
--- 10. In the first week after a customer joins the program (including their join date) they earn 2x points on all items, not just sushi - how many points do customer A and B have at the end of January?
+-- 9. If each $1 spent equates to 10 points and sushi has a 2x points multiplier 
+-- --1. how many points would each customer have?
+	SELECT
+		s.customer_id ,
+		SUM(
+			CASE
+			WHEN product_name = 'sushi' THEN price *10 *2
+			ELSE  price *10
+			END 
+		) as points
+	FROM `1_menu` m 
+	INNER JOIN `1_sales` s ON m.product_id = s.product_id 
+	GROUP BY s.customer_id 
+
+
+-- 10. In the first week after a customer joins the program (including their join date) they earn 2x points on all items, not just sushi 
+-- -- how many points do customer A and B have at the end of January?
+-- -- step 1
+	SELECT
+		s.customer_id,
+		CASE
+			WHEN s.order_date BETWEEN m2.join_date AND DATE_ADD(m2.join_date, INTERVAL 6 DAY) THEN price * 10 * 2
+			WHEN m.product_name = 'sushi' THEN m.price * 10 * 2
+			ELSE m.price * 10
+		END AS points,
+		s.order_date,
+		m2.join_date AS offer_start,
+		DATE_ADD(m2.join_date, INTERVAL 6 DAY) AS offer_end
+	FROM `1_menu` m
+	INNER JOIN `1_sales` s ON m.product_id = s.product_id
+	INNER JOIN `1_members` m2 ON m2.customer_id = s.customer_id;
+
+-- -- step 2
+	SELECT
+		s.customer_id,
+		SUM(CASE
+			WHEN s.order_date BETWEEN m2.join_date AND DATE_ADD(m2.join_date, INTERVAL 7 DAY) AND m.product_name = 'sushi' THEN m.price * 10 * 2
+			WHEN m.product_name = 'sushi' THEN m.price * 10 * 2
+			ELSE m.price * 10
+		END) AS points,
+		s.order_date,
+		m2.join_date AS offer_start,
+		DATE_ADD(m2.join_date, INTERVAL 6 DAY) AS offer_end
+	FROM `1_menu` m
+	INNER JOIN `1_sales` s ON m.product_id = s.product_id
+	INNER JOIN `1_members` m2 ON m2.customer_id = s.customer_id
+	WHERE DATE_FORMAT(s.order_date, '%Y-%m-01') = '2021-01-01'
+	GROUP BY s.customer_id 
+	
